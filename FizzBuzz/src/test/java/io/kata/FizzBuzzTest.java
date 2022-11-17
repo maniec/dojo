5784064a1a07dc14685b8ff21c9447e4eda2a5e7
gschipper <gschipper@gmail.com>
2022-11-17 23:24:20 +0100

    gradle based skeleton

---
+++ FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package io.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void fail() {
+        assertTrue(true);
+    }
+}
