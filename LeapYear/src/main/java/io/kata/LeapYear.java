489dcb15b72caab47428d6e48fc7dea449bc8a19
gschipper <gschipper@gmail.com>
2022-11-17 23:24:50 +0100

    gradle based skeleton passing initial test

---
+++ LeapYear/src/main/java/io/kata/LeapYear.java
@@ -0,0 +1,4 @@
+package io.kata;
+
+public class LeapYear {
+}
---
+++ LeapYear/src/test/java/io/kata/LeapYearTest.java
@@ -0,0 +1,14 @@
+package io.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeapYearTest {
+
+    @Test
+    void fail() {
+        assertTrue(true);
+    }
+
+}
