2138da32e39ca12be7efb5d667b6d062bf989953
gschipper <gschipper@gmail.com>
2022-11-21 00:35:32 +0100

    cycle 3 > REFACTOR keep duplications low in both code and tests

--- FizzBuzz/src/main/java/io/kata/FizzBuzz.java
+++ FizzBuzz/src/main/java/io/kata/FizzBuzz.java
@@ -3,9 +3,6 @@ package io.kata;
 public class FizzBuzz {
 
     public String convert(int number) {
-        // Principle: fake it, obvious implementation
-        if (number == 4) return "4";
-        if (number == 2) return "2";
-        return "1";
+        return String.valueOf(number);
     }
 }
--- FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
@@ -1,29 +1,17 @@
 package io.kata;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // main reference: https://www.codurance.com/katalyst/fizzbuzz#Frequently-Asked-Questions
 class FizzBuzzTest {
 
-    @Test
-    void givenNumeral1ShouldJustConvertToString1() {
-        // pick the simplest and minimal functionality
-        assertEquals("1", new FizzBuzz().convert(1));
-    }
-
-    @Test
-    void givenNumeral2ShouldJustConvertToString2() {
-        // Write a function that takes positive integers and outputs their string representation
-        assertEquals("2", new FizzBuzz().convert(2));
-    }
-
-    @Test
-    void givenNumeral4ShouldJustConvertToString4() {
-        // STILL ON: Write a function that takes positive integers and outputs their string representation
-        // prefer not to add new feature like (3 > Fizz), and still stay in original taken functionality
-        assertEquals("4", new FizzBuzz().convert(4));
+    @ParameterizedTest
+    @CsvSource({ "1,1", "2,2", "4,4" })
+    void convertNumeralToString(String expectedOutput, int actualInput) {
+        assertEquals(expectedOutput, new FizzBuzz().convert(actualInput));
     }
 
 }
