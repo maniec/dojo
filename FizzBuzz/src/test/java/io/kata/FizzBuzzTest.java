2a0700a9a487f34f224543d4b4bde091ad850db1
gschipper <gschipper@gmail.com>
2022-11-20 23:59:25 +0100

    cycle 2 > RED that exposes needed implementation

--- FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
@@ -8,8 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     @Test
-    void givenNumeral1ShouldJustConvertToString() {
+    void givenNumeral1ShouldJustConvertToString1() {
         // pick the simplest and minimal functionality
         assertEquals("1", new FizzBuzz().convert(1));
+    }
+
+    @Test
+    void givenNumeral2ShouldJustConvertToString2() {
+        // Write a function that takes positive integers and outputs their string representation.
+        assertEquals("2", new FizzBuzz().convert(2));
     }
 }
