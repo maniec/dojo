5eb30b49ea7deaa3d977c55e3bdd37e1cca5337b
gschipper <gschipper@gmail.com>
2022-11-21 00:19:13 +0100

    cycle 3 > GREEN simplest code to make it pass

--- FizzBuzz/src/main/java/io/kata/FizzBuzz.java
+++ FizzBuzz/src/main/java/io/kata/FizzBuzz.java
@@ -3,6 +3,9 @@ package io.kata;
 public class FizzBuzz {
 
     public String convert(int number) {
-        throw new UnsupportedOperationException("Implement me!");
+        // Principle: fake it, obvious implementation
+        if (number == 4) return "4";
+        if (number == 2) return "2";
+        return "1";
     }
 }
