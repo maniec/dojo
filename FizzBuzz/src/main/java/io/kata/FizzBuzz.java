0d9f32a93b279559033385f10e92f2aefccda439
gschipper <gschipper@gmail.com>
2022-11-20 23:50:43 +0100

    Revert "cycle 1 > simplest code to make it pass > GREEN"
    This reverts commit eb2f6a4022b2c68b722189992389761c1199399a.

--- FizzBuzz/src/main/java/io/kata/FizzBuzz.java
+++ FizzBuzz/src/main/java/io/kata/FizzBuzz.java
@@ -3,8 +3,6 @@ package io.kata;
 public class FizzBuzz {
 
     public String convert(int number) {
-        // implement just enough to make it pass using the simplest solution
-        // Principles: fake it until you make it, baby step, KISS, YAGNI
-        return "1";
+        throw new UnsupportedOperationException("Implement me!");
     }
 }
--- FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// main reference: https://www.codurance.com/katalyst/fizzbuzz#Frequently-Asked-Questions
 class FizzBuzzTest {
 
     @Test
     void givenNumeral1ShouldJustConvertToString() {
-        // pick the simplest and minimal functionality
-        assertEquals("1", new FizzBuzz().convert(1));
+        assertEquals(1, new FizzBuzz().convert(1));
     }
 }
