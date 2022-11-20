2115682f8a6cffe624e9f9c1df5627a5216b43c3
gschipper <gschipper@gmail.com>
2022-11-21 00:17:11 +0100

    cycle 3 > RED exposes still needs implementation

--- FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/io/kata/FizzBuzzTest.java
@@ -4,10 +4,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// main reference: https://www.codurance.com/katalyst/fizzbuzz#Frequently-Asked-Questions
 class FizzBuzzTest {
 
     @Test
-    void givenNumeral1ShouldJustConvertToString() {
-        assertEquals(1, new FizzBuzz().convert(1));
+    void givenNumeral1ShouldJustConvertToString1() {
+        // pick the simplest and minimal functionality
+        assertEquals("1", new FizzBuzz().convert(1));
     }
+
+    @Test
+    void givenNumeral2ShouldJustConvertToString2() {
+        // Write a function that takes positive integers and outputs their string representation
+        assertEquals("2", new FizzBuzz().convert(2));
+    }
+
+    @Test
+    void givenNumeral4ShouldJustConvertToString4() {
+        // STILL ON: Write a function that takes positive integers and outputs their string representation
+        // prefer not to add new feature like (3 > Fizz), and still stay in original taken functionality
+        assertEquals("4", new FizzBuzz().convert(4));
+    }
+
 }
